9b7fc23a9d5c85592ff137bf27db22925fc5a86e
nipodemos <nipodemos13@gmail.com>
2019-08-18 01:28:02 -0300

    Efeito Fade
    aprendi que em vez de utilizar a ação hide(), fica melhor usar
    a ação fadeOut(). O motivo é porque é mais suave
    o desaparecimento do texto
    outra coisa que posso fazer é colocar como parametro
    do fadeOut() é 'slow'.
    Dessa forma ele vai desaparecer mais devagar e mais suave ainda.
    O mesmo vale pro fadeIn(), só que ele vai aparecer de volta

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 $(function () {
     $('#azul').click(() => {
         $('p').css("color", "blue");
+        $('p').fadeOut();
+        $('p').delay(2000);
+        $('p').fadeIn();
     });
 
     $('#vermelho').click(() => {
         $('p').css("color", "red");
+        $('p').fadeOut('slow');
+        $('p').delay(2000);
+        $('p').fadeIn('slow');
     });
 })
