e40f0e87945e1c182e1d2b783b06c98989b2ce96
nipodemos <nipodemos13@gmail.com>
2019-08-18 01:43:07 -0300

    Encadeamento em bloco
    aprendi que eu não preciso digitar o mesmo seletor pra cada linha
    eu posso simplesmente colocando vários de uma vez!
    
    ah agora sim ficou bem melhor,
    to começando a gostar dessa tal de JQuery
    pena que nunca vou usar

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // $(seletor).ação();
 $(function () {
     $('#azul').click(() => {
-        $('p').css("color", "blue");
-        $('p').fadeOut('fast');
-        $('p').delay(2000);
-        $('p').fadeIn();
+
+        $('p')
+            .css("color", "blue")
+            .fadeOut('fast')
+            .delay(2000)
+            .fadeIn();
     });
 
     $('#vermelho').click(() => {
         $('p').css("color", "red");
-        $('#mensagem').text("Cor alterada com sucesso");
-        $('#mensagem').css('color', 'red');
-        $('#mensagem').css('border', '1px solid red');
-        $('#mensagem').delay(3000);
-        $('#mensagem').fadeOut();
+
+        $('#mensagem')
+            .text("Cor alterada com sucesso")
+            .css('color', 'red')
+            .css('border', '1px solid red')
+            .delay(3000)
+            .fadeOut();
     });
 })
